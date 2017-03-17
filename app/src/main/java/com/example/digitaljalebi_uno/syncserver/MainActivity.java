1a376dad6e526355dc5308fa25f0b0fad8b1030a
naval <navaljosh@gmail.com>
2017-03-17 18:58:11 +0530

    update for LVMH

--- app/src/main/java/com/example/digitaljalebi_uno/syncserver/MainActivity.java
+++ app/src/main/java/com/example/digitaljalebi_uno/syncserver/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.digitaljalebi_uno.syncserver;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
@@ -26,18 +27,28 @@ import java.util.Enumeration;
  * <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
  */
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     TextView infoIp, infoPort;
 
-    static final int SocketServerPORT = 8080;
+    static final int SocketServerPORT = 8880;
     ServerSocket serverSocket;
 
     ServerSocketThread serverSocketThread;
+    File file= new File(android.os.Environment.getExternalStorageDirectory(),"lvmh");
+    public int h = file.listFiles().length+1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+       // verifyStoragePermissions(getCallingActivity());
         setContentView(R.layout.activity_main);
         infoIp = (TextView) findViewById(R.id.infoip);
         infoPort = (TextView) findViewById(R.id.infoport);
@@ -46,6 +57,7 @@ public class MainActivity extends ActionBarActivity {
 
         serverSocketThread = new ServerSocketThread();
         serverSocketThread.start();
+     //   Log.d("naval", "v"+Environment.getExternalStorageDirectory());
     }
 
     @Override
@@ -61,7 +73,19 @@ public class MainActivity extends ActionBarActivity {
             }
         }
     }
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
     private String getIpAddress() {
         String ip = "";
         try {
@@ -110,10 +134,13 @@ public class MainActivity extends ActionBarActivity {
                     }});
 
                 while (true) {
-                    Log.d("naval", "connected");
+                    Log.d("naval", "waiting for connection");
                     socket = serverSocket.accept();
+                    Log.d("naval", "Got connection");
                     FileTxThread fileTxThread = new FileTxThread(socket);
+                    Log.d("naval", "created file thread");
                     fileTxThread.start();
+                    Log.d("naval", "started file thread");
                 }
             } catch (IOException e) {
                 // TODO Auto-generated catch block
@@ -133,87 +160,85 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public class FileTxThread extends Thread {
+        private  ServerSocket serverSocket;
+        private  Socket clientSocket;
+        private  InputStream inputStream;
+        private  FileOutputStream fileOutputStream;
+        private  BufferedOutputStream bufferedOutputStream;
+        private  int filesize = 10000000; // filesize temporary hardcoded
+        private  int bytesRead;
+        private  int current = 0;
         Socket socket;
 
         FileTxThread(Socket socket){
             this.socket= socket;
+            Log.d("naval", "constructor file thread initialized");
         }
 
-       /* public File getNewestFileInDirectory() {
-            File newestFile = null;
-
-            // start loop trough files in directory
-            File file = new File(filePath);
-            if (newestFile == null || file.lastModified().after(newestFile.lastModified())) {
-                newestFile = file;
-            }
-            // end loop trough files in directory
-
-            return newestFile;
-        }*/
-
-        public  File lastFileModified() {
-            String dir = "/storage/emulated/0/naval/";
-            File fl = new File(dir);
-            File[] files = fl.listFiles(new FileFilter() {
-                public boolean accept(File file) {
-                    return file.isFile();
-                }
-            });
-            long lastMod = Long.MIN_VALUE;
-            File choice = null;
-            for (File file : files) {
-                if (file.lastModified() > lastMod) {
-                    choice = file;
-                    lastMod = file.lastModified();
-                }
-            }
-            return choice;
-        }
         @Override
         public void run() {
 
-           /* File file = new File(
-                    Environment.getExternalStorageDirectory(),
-                    "naval.png");*/
-            File file = lastFileModified();
-
-            byte[] bytes = new byte[(int) file.length()];
-            BufferedInputStream bis;
+            Log.d("naval", "Run function  file thread");
+            byte[] mybytearray = new byte[filesize];    //create byte array to buffer the file
             try {
-                bis = new BufferedInputStream(new FileInputStream(file));
-                bis.read(bytes, 0, bytes.length);
+                inputStream = socket.getInputStream();
+                Log.d("naval", "setting input stream");
 
-                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-                oos.writeObject(bytes);
-                oos.flush();
+                // check for SD card mounted
+                if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()))
+                {
+                    Log.d("naval", "Mounted");
+                }
 
-                socket.close();
 
-                final String sentMsg = "File sent to: " + socket.getInetAddress();
-                MainActivity.this.runOnUiThread(new Runnable() {
 
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this,
-                                sentMsg,
-                                Toast.LENGTH_LONG).show();
-                    }});
+               // Log.d("naval", "Tab file location :" + path);
 
-            } catch (FileNotFoundException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } finally {
+
                 try {
-                    socket.close();
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
+                    if(h>=29)// for LVMH we will save only 28 images and then we will replace them
+                        h=0;
+                    fileOutputStream = new FileOutputStream(Environment.getExternalStorageDirectory() + "/lvmh/"+h+"img.jpg");
+
+                }catch (IOException e)
+                {
                     e.printStackTrace();
                 }
+                Log.d("naval", "setting path for file");
+                bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+                h++;// count increase for file name
+                Log.d("naval-count pic","val"+h);
+
+                System.out.println("Receiving...");
+                Log.d("naval", "Receiving");
+
+                //following lines read the input slide file byte by byte
+                bytesRead = inputStream.read(mybytearray, 0, mybytearray.length);
+                current = bytesRead;
+
+                do {
+                    bytesRead = inputStream.read(mybytearray, current, (mybytearray.length - current));
+                    if (bytesRead >= 0) {
+                        current += bytesRead;
+                    }
+                } while (bytesRead > -1);
+
+
+                bufferedOutputStream.write(mybytearray, 0, current);
+                bufferedOutputStream.flush();
+                bufferedOutputStream.close();
+                inputStream.close();
+                //clientSocket.close();
+                //serverSocket.close();
+
+                System.out.println("Sever recieved the file");
+                Log.d("naval","server received file");
             }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
 
         }
     }
